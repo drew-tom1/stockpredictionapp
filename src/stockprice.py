0806b3b34ab2081660bc4b8673f07ac5729b38aa
drew-tom1 <tom.drew261@gmail.com>
2024-08-13 22:29:54 -0700

    revised predict endpoint, reduced training and changed tools.

--- src/stockprice.py
+++ src/stockprice.py
@@ -1,25 +1,44 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import RobustScaler
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout
-from fastapi import FastAPI
+from tensorflow.keras.layers import LSTM, Dense, Input
+from fastapi import FastAPI, HTTPException
 import uvicorn
 from pydantic import BaseModel
 from datetime import datetime, timedelta
+from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class StockRequest(BaseModel):
     ticker: str
     start_date: str
     end_date: str
+    future_days: Optional[int] = 7
 
 def get_stock_data(ticker, start_date, end_date):
-    data = yf.download(ticker, start=start_date, end=end_date)
-    return data['Close'].values.reshape(-1, 1)
+    try:
+        data = yf.download(ticker, start=start_date, end=end_date)
+        if data.empty:
+            raise ValueError("No data found for the ticker symbol")
+        return data['Close'].values.reshape(-1, 1)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="An error occured while retrieving data, please try again later.")
 
 def create_sequences(data, look_back):
     X, y = [], []
@@ -40,12 +59,12 @@ async def history(request: StockRequest):
 
 @app.post('/predict')
 async def predict(request: StockRequest):
-    stock_data = get_stock_data(request.ticker, '2010-01-01', request.end_date)
+    stock_data = get_stock_data(request.ticker, request.start_date, request.end_date)
     
-    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaler = RobustScaler()
     normalized_data = scaler.fit_transform(stock_data)
     
-    look_back = 100
+    look_back = 60
     X, y = create_sequences(normalized_data, look_back)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -54,20 +73,19 @@ async def predict(request: StockRequest):
     X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
     model = Sequential()
-    model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])))
-    model.add(Dropout(0.2))
+    model.add(Input(shape=(X_train.shape[1], X_train.shape[2])))
     model.add(LSTM(units=50, return_sequences=True))
-    model.add(Dropout(0.2))
+    model.add(LSTM(units=50, return_sequences=True))
     model.add(LSTM(units=50, return_sequences=False))
     model.add(Dense(units=1))
     model.compile(optimizer='adam', loss='mean_squared_error')
 
-    model.fit(X_train, y_train, epochs=30, batch_size=32)
+    model.fit(X_train, y_train, epochs=15, batch_size=32)
 
     predictions = []
     last_sequence = X[-1]
 
-    for _ in range(5):  # Predict the next 5 business days
+    for _ in range(request.future_days):  # predict the future days
         prediction = model.predict(last_sequence.reshape(1, look_back, 1))
         predictions.append(prediction[0, 0])
         
@@ -76,7 +94,7 @@ async def predict(request: StockRequest):
     predictions = scaler.inverse_transform(np.array(predictions).reshape(-1, 1))
 
     last_date = datetime.strptime(request.end_date, '%Y-%m-%d')
-    future_dates = pd.date_range(start=last_date + timedelta(days=1), periods=5, freq='B')
+    future_dates = pd.date_range(start=last_date + timedelta(days=1), periods=request.future_days, freq='B')
 
     df_chart = pd.DataFrame({'Predicted': predictions.flatten()}, index=future_dates)
 
