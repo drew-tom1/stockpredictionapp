7e4c8a5535b2d1b4a6570f65c1f476e7b661a8d6
drew-tom1 <tom.drew261@gmail.com>
2024-10-06 21:26:50 -0700

    changed server.py to 8080

--- src/server.py
+++ src/server.py
@@ -141,4 +141,4 @@ def get_headlines(request: TickerRequest = Depends(validate_query)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
